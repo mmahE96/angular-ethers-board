969c63945543a33eff9bb65fc6920167fb9950e2
Mahir Mulaomerovic <mahir@Mahirs-MacBook-Pro.local>
2021-12-28 16:41:10 +0100

    same level of functionality as react board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -25,7 +26,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import {ethers} from "ethers"
 
 declare let window:any;
@@ -18,26 +19,55 @@ export class BlockComponent {
     public number;
     public parentHash;
     public timestamp;
+    public balance;
+    public address;
+    public transactionCount;
+    public blockNumber;
  
   constructor(){
-      this.provider = new ethers.providers.Web3Provider(window.ethereum)
+      this.provider = new ethers.providers.Web3Provider(window.ethereum);
   }  
 
  async getBlock(){
-     const blockOne = await this.provider.getBlock()
-     this.block = blockOne
-     this.difficulty= blockOne.difficulty
-  this.hash= blockOne.hash
-  this.miner= blockOne.miner
-  this.nonce= blockOne.nonce
-  this.number= blockOne.number
-  this.parentHash= blockOne.parentHash
-  this.timestamp= blockOne.timestamp
-     console.log(this.block)
-     return 
-    
+     const blockOne = await this.provider.getBlock();
+
+     this.block = blockOne;
+     this.difficulty= blockOne.difficulty;
+     this.hash= blockOne.hash;
+     this.miner= blockOne.miner;
+     this.nonce= blockOne.nonce;
+     this.number= blockOne.number;
+     this.parentHash= blockOne.parentHash;
+     this.timestamp= blockOne.timestamp;
+     console.log(this.block);
+     return     
   }
 
+  
+  onSubmit(form: NgForm) {
+    console.log(form.form.value.addressName)
+   this.address = form.form.value.addressName
+   this.getBalance(this.address)
+   this.getCount(this.address)
+  }
+
+  async getBalance(address){
+    const bal = await (await this.provider.getBalance(address))._hex;
+    this.balance = bal
+  }
+
+  async getCount(address) {
+    const count = await this.provider.getTransactionCount(address);
+    this.transactionCount = count
+  }
+
+  async getBlockNumber(){
+    const blockNumber = await this.provider.getBlockNumber()
+    this.blockNumber = blockNumber;
+    return
+
+  }
+  
 
 
 }
