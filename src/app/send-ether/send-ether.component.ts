2f1cb9ac4fe77c01fc2e9170e5827d08b6f274f1
Mahir Mulaomerovic <mahir.cs@hotmail.com>
2022-01-11 16:03:42 +0100

    Added send transaction route

--- src/app/send-ether/send-ether.component.ts
+++ src/app/send-ether/send-ether.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {ethers} from "ethers";
+import { NgForm } from '@angular/forms';
+
+declare let window:any;
 
 @Component({
   selector: 'app-send-ether',
   templateUrl: './send-ether.component.html',
   styleUrls: ['./send-ether.component.css']
 })
-export class SendEtherComponent implements OnInit {
+export class SendEtherComponent{
+  public provider;
+  public signer;
+  public transactionPrice = "No data";
+  public transactionError = "No data";
+  public transactionSuccess = "No data";
+  public from= "no data"
+  public hash = "no data"
+  public value = "no data"
+  public nonce= "no data"
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.provider = new ethers.providers.Web3Provider(window.ethereum)
+    this.signer = this.provider.getSigner()
   }
+
+  onSubmit(form: NgForm){
+   this.sendEther(form.form.value.address, form.form.value.amount)
+   }
+
+  async sendEther (address, amount){
+try {
+  const tx = await this.signer.sendTransaction({
+    to: address,
+    value: ethers.utils.parseEther(amount)    
+
+});
+
+this.transactionSuccess = tx
+this.from=tx.from
+this.hash=tx.hash
+this.nonce=tx.nonce
+
+this.value=tx.value.toString()
+console.log(tx)
+  
+} catch (error) {
+  this.transactionError = error.message
+  
+}
+   
+
+  } 
+
+  
 
 }
